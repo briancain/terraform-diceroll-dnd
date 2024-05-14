bf85d942ce64a1f8222c7fc75bc1da711b3e42af
Brian Cain <bcain@hashicorp.com>
2024-05-14 15:14:31 -0700

    Bump to latest provider version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     diceroll = {
       source  = "briancain/diceroll"
-      version = ">=0.1.5"
+      version = ">=0.1.10"
     }
     random = {
       source  = "hashicorp/random"
