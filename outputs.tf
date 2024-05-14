889cb801890b994d9bfd7994510641d33651f3dd
Brian Cain <bcain@hashicorp.com>
2024-05-14 13:27:27 -0700

    Add two output values

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "dnd_roll" {
   value = diceroll_roll.dnd
 }
+
+output "roll_result" {
+  value = diceroll_roll.dnd.result
+}
+
+output "calculated_total" {
+  value = diceroll_roll.dnd.calculated_total
+}
