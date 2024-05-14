1ce709c4f5f686920dc19900bb5c632b00befb60
Brian Cain <bcain@hashicorp.com>
2024-05-14 14:18:33 -0700

    Generate a random seed every run

--- main.tf
+++ main.tf
@@ -4,11 +4,20 @@ terraform {
       source  = "briancain/diceroll"
       version = ">=0.1.5"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.6.1"
+    }
   }
 }
 
 resource "diceroll_roll" "dnd" {
   quantity = 4
   sides    = 20
-  seed     = var.die_seed
+  seed     = try(var.die_seed, random_string.random.result)
+}
+
+resource "random_string" "random" {
+  length           = 16
+  special          = false
 }
